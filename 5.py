4407a9cd0ab042405867a8c87e4e0939d4f1271b
andriibutsUA <andriibuts@gmail.com>
2017-02-03 16:54:34 +0200

    task 5 fix

--- 5.py
+++ 5.py
@@ -1,4 +1,17 @@
 def srt(versions):
-    return sorted(versions)
 
-print(srt(['1.8.2.4', '1.5.3.45', '1.6.1', '1.2.8.47', '1.2.9', '2.0']))
+    def to_float(n):
+        temp = ''
+        if '.' in n:
+            index_of_dot = (n.index('.'))
+            temp += n[:index_of_dot+1]
+            for k in range(index_of_dot, len(n)):
+                if n[k] != '.':
+                    temp += n[k]
+            return float(temp)
+        else:
+            return float(n)
+    versions.sort(key=to_float)
+    return versions
+
+print(srt(['22', '22.5', '5', '1.8.2.4', '1.5.3.45', '1.6.1', '1.2.8.47', '1.2.9', '2.0']))
