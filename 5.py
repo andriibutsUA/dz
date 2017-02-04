0b85c351677a10995faa1e0df49880f44a013089
andriibutsUA <andriibuts@gmail.com>
2017-02-04 08:35:57 +0200

    task 5 with list comprehension

--- 5.py
+++ 5.py
@@ -1,17 +1,7 @@
-def srt(versions):
+def sort_versions(v):
+    return [int(u) for u in v.split('.')]
 
-    def to_float(n):
-        temp = ''
-        if '.' in n:
-            index_of_dot = (n.index('.'))
-            temp += n[:index_of_dot+1]
-            for k in range(index_of_dot, len(n)):
-                if n[k] != '.':
-                    temp += n[k]
-            return float(temp)
-        else:
-            return float(n)
-    versions.sort(key=to_float)
-    return versions
+versions = ['1.22.8.47', '1.2.9', '1.5.3.45', '1.6.1', '1.8.2.4', '2.0']
 
-print(srt(['22', '22.5', '5', '1.8.2.4', '1.5.3.45', '1.6.1', '1.2.8.47', '1.2.9', '2.0']))
+versions.sort(key=sort_versions)
+print(versions)
