89537cd8a709ece023413250b1fd0ab7b4a21bf1
andriibutsUA <andriibuts@gmail.com>
2017-02-04 08:43:02 +0200

    task 5

--- 5.py
+++ 5.py
@@ -1,5 +1,8 @@
 def sort_versions(v):
-    return [int(u) for u in v.split('.')]
+    my_list = []
+    for i in v.split('.'):
+        my_list.append(int(i))
+    return my_list
 
 versions = ['1.22.8.47', '1.2.9', '1.5.3.45', '1.6.1', '1.8.2.4', '2.0']
 
